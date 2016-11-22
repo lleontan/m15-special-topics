2a9d38670066342f1d1c31cce7e586b076fdb653
lleontan <lleontan@gmail.com>
2016-11-22 14:40:45 -0800

    added things

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,6 +1,8 @@
 # Exercise-1
 # Implement code from this book chapter: http://r4ds.had.co.nz/many-models.html
 
+#https://github.com/lleontan/m15-special-topics/tree/master/exercise-1
+
 # Packages
 # install.packages('modelr')
 # install.packages('tidyverse')
@@ -8,11 +10,32 @@
 library(gapminder)
 library(modelr)
 library(tidyverse)
+library(ggplot2)
 
 # Initial view of the data with ggplot
-
-
+gapminder %>% 
+  ggplot(aes(year, lifeExp, group = country)) +
+  geom_line(alpha = 1/3)
 # Look only at new zealand
+nz <- filter(gapminder, country == "New Zealand")
+nz %>% 
+  ggplot(aes(year, lifeExp)) + 
+  geom_line() + 
+  ggtitle("Full data = ")
+
+nz_mod <- lm(lifeExp ~ year, data = nz)
+nz %>% 
+  add_predictions(nz_mod) %>%
+  ggplot(aes(year, pred)) + 
+  geom_line() + 
+  ggtitle("Linear trend + ")
+
+nz %>% 
+  add_residuals(nz_mod) %>% 
+  ggplot(aes(year, resid)) + 
+  geom_hline(yintercept = 0, colour = "white", size = 3) + 
+  geom_line() + 
+  ggtitle("Remaining pattern")
 
 
 
@@ -21,12 +44,18 @@ library(tidyverse)
 
 # Nest the data by country/continent
 
+by_country <- gapminder %>% 
+  group_by(country, continent) %>% 
+  nest()
 
 
 # Define a statistical model, and store it in a function
-
+country_model <- function(df) {
+  lm(lifeExp ~ year, data = df)
+}
 
 # Use the `map` functionality to run the same model for each country separately
+models <- map(by_country$data, country_model)
 
 
 # Add additional columns to store your residuals (distance between data and prediction)
